8531c3e8d12cd9cd287b55ad76d47d7b0985a6fa
Momen <momen.devv@gmail.com>
2025-07-23 03:15:05 +0300

    zod and middleware

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
-import express from "express"; 
-import cuisines from './routes/cuisines.js'
-import restaurants from "./routes/restaurants.js"
+import express from 'express';
+import cuisines from './routes/cuisines.js';
+import restaurants from './routes/restaurants.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
-const PORT= process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.use("/cuisines", cuisines);
-app.use("/restaurants", restaurants);
+app.use('/cuisines', cuisines);
+app.use('/restaurants', restaurants);
 
-app.listen(PORT, () => {
+app.use(errorHandler);
+
+app
+  .listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-}).on("error", (err) => {
-    throw new Error (err.message);
-})
+  })
+  .on('error', (err) => {
+    throw new Error(err.message);
+  });
--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express from 'express';
+import { validate } from '../middlewares/validate.js';
+import { RestaurantSchema, type Restaurant } from '../schemas/restaurantValidation.js';
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Restaurants");
+router.post('/', validate(RestaurantSchema), (req, res) => {
+  const data = req.body as Restaurant;
+  res.send('Restaurants');
 });
 
 export default router;
